98f17561b3a57fc4f3cc1ed1e9f51513d8e75d5b
abiola-sodiq <rasababe01@gmail.com>
2023-07-03 14:15:05 +0100

    first commit

---
+++ src/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Components/Header'
+import Main from './Components/Main'
+
+const LandingPage = () => {
+  return (
+    <div className=' w-screen h-[100svh]'>
+        <Header/>
+        <Main />
+    </div>
+  )
+}
+
+export default LandingPage
